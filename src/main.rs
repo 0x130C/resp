c1598a64b9b7b3e5ad07cca5004bcd02313ad3bf
Felix Truong <felixtruong@hotmail.com>
2018-08-18 12:09:27 +0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,13 @@
-extern crate actix_web;
-extern crate listenfd;
-extern crate chrono;
-#[macro_use]
-extern crate tera;
-#[macro_use]
-extern crate diesel;
-#[macro_use]
-extern crate diesel_derive_enum;
-extern crate dotenv;
-extern crate serde;
-extern crate serde_json;
-#[macro_use]
-extern crate serde_derive;
-#[macro_use]
-extern crate validator_derive;
-extern crate validator;
+extern crate resp;
 extern crate env_logger;
-extern crate uuid;
-extern crate futures;
-#[macro_use] extern crate log;
-
-mod db;
-mod schema;
-mod utils;
-mod models;
-mod views;
-mod controllers;
-mod middlewares;
-mod extractors;
-
-use listenfd::ListenFd;
-use actix_web::actix::*;
-use actix_web::{server, middleware, App};
-use actix_web::{error, Error, Responder, Result};
-use actix_web::{http, HttpRequest, HttpResponse};
-use actix_web::fs::{self, NamedFile};
-use actix_web::pred;
-use utils::http::redirect;
-use utils::views::render_template;
-
-use views::login;
-
-use db::{create_pool, DbExecutor};
-
-pub struct AppState {
-    template: tera::Tera, // <- store tera template in application state
-    db: Addr<DbExecutor>
-}
-
-
-
-fn index(req: &HttpRequest<AppState>) -> Result<HttpResponse, Error> {
-    render_template(&req, "index.html")
-}
-
-
-fn get_tera_template() -> tera::Tera {
-    let mut tera:tera::Tera = compile_templates!("templates/**/*");
-    //TODO: Create function addMinInProduct to add .min in assets file, ex: app.css -> app.min.css
-    //        tera.register_global_function("now", tera::builtins::global_functions::make_now_fn())
-    tera
-}
+extern crate dotenv;
+use resp::{Server, Config};
 
 fn main() {
     dotenv::dotenv().ok();
     ::std::env::set_var("RUST_LOG", "actix_web=error");
     env_logger::init();
 
-    let sys = System::new("resp-server");
-
-    let mut listenfd = ListenFd::from_env();
-
-    let pool = create_pool();
-    let addr = SyncArbiter::start(3, move || DbExecutor(pool.clone()));
-
-    let mut server = server::new(move || {
-
-        let mut apps = vec![
-            App::with_state(AppState{ template:get_tera_template(), db: addr.clone() })
-                .middleware(middleware::Logger::default())
-                .resource("/", |r| {
-                    r.name("homepage");
-                    r.f(index)
-                })
-                .resource("/login", |r| {
-                    r.method(http::Method::GET).f(login::get);
-                    r.method(http::Method::POST).f(login::post);
-                })
-                .handler("/", fs::StaticFiles::new("./static").unwrap())
-                .boxed()
-        ];
-        if let Some(_) = option_env!("DEV_MODE") {
-            apps.insert(0,
-                App::new()
-                    .prefix("/client")
-                    .handler("/", fs::StaticFiles::new("./client").unwrap().show_files_listing())
-                    .boxed()
-            );
-        }
-        apps
-    });
-
-
-    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
-        server.listen(l)
-    } else {
-        server.bind("127.0.0.1:8080").unwrap()
-    };
-    server.start();
-
-    println!("Started http server.");
-    let _ = sys.run();
+    let server = Server::new(Config{});
+    ::std::process::exit(server.start());
 }
--- src/middlewares.rs
+++ src/middlewares.rs
@@ -1,62 +1,11 @@
-use std::io::{Error as IoError, ErrorKind};
-use futures::Future;
-use futures::future;
-use actix_web::{HttpRequest, Error};
-use actix_web::middleware::session::RequestSession;
+use actix_web::middleware::{Middleware};
+use actix_web::{HttpRequest};
 
-use AppState;
-use models::users::{UserLookup, User};
+pub struct Passport;
 
-pub type UserAuthenticationResult = Box<Future<Item = User, Error = Error>>;
 
-pub trait UserAuthentication {
-    fn is_authenticated(&self) -> bool;
-    fn user(&self) -> UserAuthenticationResult;
-}
-impl UserAuthentication for HttpRequest<AppState> {
-    #[inline(always)]
-    fn is_authenticated(&self) -> bool {
-        match self.session().get::<i32>("uid") {
-            Ok(session) => {
-                match session {
-                    Some(_session_id) => true,
-                    None => false
-                }
-            },
+impl Middleware<S> for Passport {
+    fn start(&self, req: &mut HttpRequest<S>) -> Result {
 
-            Err(e) => {
-                error!("Error'd when attempting to fetch session data: {:?}", e);
-                false
-            }
-        }
-    }
-
-    fn user(&self) -> UserAuthenticationResult {
-        match self.session().get::<String>("uid") {
-            Ok(session) => { match session {
-                Some(session_id) => {
-                    Box::new(self.state().db.send(UserLookup {
-                        session_id: String::from(session_id)
-                    }).from_err().and_then(|res| match res {
-                        Ok(user) => Ok(user),
-                        Err(err) => {
-                            // Temporary because screw matching all these error types
-                            let e = IoError::new(ErrorKind::NotFound, format!("{}", err));
-                            Err(e.into())
-                        }
-                    }))
-                },
-
-                None => {
-                    let e = IoError::new(ErrorKind::NotFound, "User has no session data.");
-                    Box::new(future::err(e.into()))
-                }
-            }},
-
-            Err(e) => {
-                error!("Error'd when attempting to fetch session data: {:?}", e);
-                Box::new(future::err(e.into()))
-            }
-        }
     }
 }
